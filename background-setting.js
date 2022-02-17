b3dc5d78f22c6ec850c8b428716b866ff25522de
adit3455 <88522322+adit3455@users.noreply.github.com>
2022-02-17 17:13:53 +0700

    Add files via upload

---
+++ background-setting.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+    el: "#container-background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 100.00,
+    minWidth: 100.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xf2f2f2,
+    backgroundColor: 0x0
+  })
